dbba7fb6e2b6016890860b02d303dda9d298b0bc
Kiddie22 <wrasula@gmail.com>
2023-11-12 18:35:49 +0530

    feat: save mp3, add file type validation and error handling

--- audiofy-cli.py
+++ audiofy-cli.py
@@ -3,7 +3,6 @@ import pyttsx3
 
 '''
 TODO
-Save audio as file
 Add option to select variable volumes, voices, speeds
 '''
 
@@ -11,12 +10,16 @@ Add option to select variable volumes, voices, speeds
 
 while (True):
     fileName = input(
-        "Place your pdf file in the /books folder and enter the file name with the extension: \n")
+        "Place your pdf file in the /books folder and enter file name (.pdf extension not needed): ")
+    if fileName.endswith('.pdf'):
+        fileName = fileName[:-4]
     try:
-        reader = PdfReader(f'books/{fileName}')
+        reader = PdfReader(f'books/{fileName}.pdf')
         break
     except FileNotFoundError:
         print("Could not find a file with that name. Please try again...")
+    except:
+        print("Unsupported file type. Please make sure you have a pdf.")
 
 number_of_pages = len(reader.pages)
 
@@ -28,11 +31,10 @@ for page_number in range(number_of_pages):
     clean_text = text.strip().replace('\n', ' ')
     full_text += clean_text
 
-# read extracted text
+# convert text to mp3
 
 engine = pyttsx3.init()
-engine.say(full_text)
+engine.connect('started-utterance', print('Converting....'))
+engine.connect('finished-utterance', print(f'Conversion complete! File saved as {fileName}.mp3'))
+engine.save_to_file(full_text, f"{fileName}.mp3")
 engine.runAndWait()
-
-print("Finished reading...")
-print("Exiting application")

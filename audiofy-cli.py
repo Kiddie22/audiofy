29546601a7585c1dcfcbf2e4a26e142dfca8b8d6
RasulaYadithya <rasula@surge.global>
2023-11-12 18:02:55 +0530

    feat: create initial script

---
+++ audiofy-cli.py
@@ -0,0 +1,32 @@
+from PyPDF2 import PdfReader
+import pyttsx3
+
+# extract pdf text
+
+while(True):
+    fileName = input(
+        "Place your pdf file in the /books folder and enter the file name with the extension: \n")
+    try:
+        reader = PdfReader(f'books/{fileName}')
+        break
+    except FileNotFoundError:
+        print("Could not find a file with that name. Please try again...")
+
+number_of_pages = len(reader.pages)
+
+full_text = ''
+
+for page_number in range(number_of_pages):
+    page = reader.pages[page_number]
+    text = page.extract_text()
+    clean_text = text.strip().replace('\n', ' ')
+    full_text += clean_text
+
+# read extracted text
+
+engine = pyttsx3.init()
+engine.say(full_text)
+engine.runAndWait()
+
+print("Finished reading...")
+print("Exiting application")
